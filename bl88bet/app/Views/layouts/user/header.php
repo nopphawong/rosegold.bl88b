9e5af883525a3392832e3062c834feb71f5d9e64
AlexKhonshu <nopphawong.n@gmail.com>
2023-07-10 17:19:43 +0700

    rewrite user page

--- bl88bet/app/Views/layouts/user/header.php
+++ bl88bet/app/Views/layouts/user/header.php
@@ -7,16 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= esc($title) ?></title>
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
-
-    <?php
-    $uri = \Config\Services::uri();
-    $request = \Config\Services::request();
-    ?>
-    <?php if (trim($uri->getPath()) != '/') : ?>
-    <?php endif; ?>
-
+    <!-- Bootstrap 4 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
     <!-- Rose Gold CSS -->
     <?= link_tag('assets/css/rosegold.css?v=1.1.4') ?>
     <!-- Custom Bootrap -->
